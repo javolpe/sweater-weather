830446bb55679e1f5524a7e8b8a682b9c4d7862b
Jake Volpe <volpe.jacob@gmail.com>
2021-04-23 17:56:01 -0600

    Setup mapquest service & test, pass test

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,15 @@
+class MapquestService 
+  def self.conn 
+    connection = Faraday.new(url: "http://www.mapquestapi.com") do |req|
+      req.params['key'] = ENV['MAP_KEY']
+    end
+  end 
+
+  def self.find_lat_long(city)
+    response = conn.get("/geocoding/v1/address") do |req|
+      req.params['location'] = city
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
---
+++ spec/services/mapquest_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe MapquestService, type: :model do 
+  describe "happy path" do 
+    describe "find lat long happy path" do 
+      it "should return Denvers lat/lang if given Denver,CO as query params" do 
+        response = MapquestService.find_lat_long("denver,co")
+
+        expect(response.class).to eq(Hash)
+        expect(response).to have_key(:results)
+        expect(response[:results]).to be_an(Array)
+        expect(response[:results].first).to have_key(:locations)
+        expect(response[:results].first[:locations]).to be_an(Array)
+        expect(response[:results].first[:locations].first).to have_key(:latLng)
+        expect(response[:results].first[:locations].first[:latLng]).to be_a(Hash)
+        expect(response[:results].first[:locations].first[:latLng]).to have_key(:lat)
+        expect(response[:results].first[:locations].first[:latLng]).to have_key(:lng)
+        expect(response[:results].first[:locations].first[:latLng][:lat]).to be_a(Float)
+        expect(response[:results].first[:locations].first[:latLng][:lng]).to be_a(Float)
+      end
+    end
+  end
+end
