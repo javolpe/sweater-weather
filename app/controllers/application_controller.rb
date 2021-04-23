414b7ad858f9defd6f2649d3d8c599a4a35751b3
Jake Volpe <volpe.jacob@gmail.com>
2021-04-23 17:30:42 -0600

    Add gems, add routes, map service test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_unprocessable_entity_response
+
+  def invalid_params
+    render json: { data: {}, error: 'invalid parameters' }, status: :bad_request
+  end
 end
