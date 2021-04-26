d663933cd8dafbb1ce39a2d602f6f0be06631350
Jake Volpe <volpe.jacob@gmail.com>
2021-04-26 11:33:26 -0600

    Added sad paths in request spec

--- spec/requests/api/v1/salaries/salaries_index_spec.rb
+++ spec/requests/api/v1/salaries/salaries_index_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'API::V1::Salaries#Inddex', type: :request do
       get api_v1_salaries_path, params: {destination: "miami"}
 
       json = JSON.parse(response.body, symbolize_names: true)
+      expect(response.status).to eq(200)
       
       expect(json[:data]).to be_a(Hash)
       expect(json[:data][:id]).to be_nil 
@@ -33,6 +34,24 @@ RSpec.describe 'API::V1::Salaries#Inddex', type: :request do
       expect(json[:data][:attributes][:salaries].first[:title]).to be_a(String)
       expect(json[:data][:attributes][:salaries].first[:min]).to be_a(String)
       expect(json[:data][:attributes][:salaries].first[:max]).to be_a(String)
+    end
+  end
+  describe 'sad path' do
+    it 'returns invalid params if params[:location] isnt present', :vcr do 
+      get api_v1_salaries_path
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response.status).to eq(400)
+      
+      expect(json[:error]).to eq("invalid parameters")
+    end
+    it 'returns invalid params if params[:location] is not a string', :vcr do 
+      get api_v1_salaries_path, params: {location: ""}
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response.status).to eq(400)
+      
+      expect(json[:error]).to eq("invalid parameters")
     end
   end
 end
