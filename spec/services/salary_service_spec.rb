0e2aa64d83277931d5c5ee65a34981baa143e78c
Jake Volpe <volpe.jacob@gmail.com>
2021-04-26 09:58:10 -0600

    Still trying to get to salaries

--- spec/services/salary_service_spec.rb
+++ spec/services/salary_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SalaryService, type: :model do
   describe "happy path" do 
     describe "when given a city" do 
       it "returns the salary data we need" do 
-        response = SalaryService.find_city_info("miami")
+        response = SalaryService.find_urban_area_salaries("miami")
 
         binding.pry
       end
