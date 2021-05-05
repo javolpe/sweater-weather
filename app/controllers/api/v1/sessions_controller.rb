8a9a033c4ec8f3b22db824652a45a76e9e8f2b85
Jake Volpe <volpe.jacob@gmail.com>
2021-05-05 10:13:45 -0600

    Fix sessions controller create typo

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create 
-    return bad_login if !params[:downcase] && !params[:email]
+    return bad_login if !params[:password] || !params[:email]
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       @serial = UsersSerializer.new(user)
